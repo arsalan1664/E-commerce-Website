4150f588f26960adc8814d978183d70f699bdd86
arsalan ali <arsalan1664@gmail.com>
2023-06-22 14:33:53 +0500

    api working

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,43 @@
 import TextField from '@mui/material/TextField';
-import { Box, Button } from '@mui/material';
+import { Alert, Box, Button, Collapse, IconButton, Snackbar } from '@mui/material';
 import Layout from '../layout/Layout';
 import './Login.css'
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+import CloseIcon from '@mui/icons-material/Close';
+
+
 
 export default function Login() {
+
+  const [email,setEmail] = useState('')
+  const [passward,setPassward] = useState('')
+  const [open,setOpen] = useState(false)
+  const navigate = useNavigate()
+
+  
+ 
+ const AlertClick = () => setOpen(true)
+   
+  const handleSubmit = async(e) => {
+      e.preventDefault();
+      try {
+        const res = await axios.post(`${process.env.REACT_APP_API}api/v1/auth/login`,{email,passward}
+        )
+        if(res.data.success){
+          AlertClick() 
+          navigate('/')
+        }
+      } catch (error) {
+        console.log(error)
+      }
+
+
+      
+       
+  }
+
   return (
     <Layout>
     <div className='login__form'>
@@ -29,6 +63,8 @@ export default function Login() {
           type="email"
           autoComplete="current-password"
           variant="standard"
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
         />
          <TextField
           id="standard-password-input"
@@ -36,11 +72,34 @@ export default function Login() {
           type="password"
           autoComplete="current-password"
           variant="standard"
+          value={passward}
+          onChange={(e)=>setPassward(e.target.value)}
         />
          
-        <Button type='Submit' variant="contained">Submit</Button>
+         <Button  onClick={handleSubmit} type='submit' variant="contained" >Submit</Button>
         
       </div>
+      <Snackbar open={open}> 
+        <Collapse in={open}>
+        <Alert
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+        >
+          Login Successfully
+        </Alert>
+      </Collapse>
+        </Snackbar>
     </Box>
     </div>
     </Layout>
